9cfcbc6ac4c587ddcf8c1a013b4cee2e9cf68441
CHUANHSIN CHEN <cindychen@CHUANHSINdeMacBook-Pro.local>
2022-01-04 21:42:24 +0800

    React Classes: Convert the single-line JSX into a React component instantiated from a React class

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,24 @@
 	---- JSX is converted to JavaScript with Babel compiler----
 	---- Rendering : Transform virtual DOM (i.e. React elements) to real DOM ----
 */
-const continents = ['Africa', 'America', 'Asia', 'Australia', 'Europe'];
-const helloContinents = Array.from(continents, c=> `Hello ${c}!`);
-const message = helloContinents.join(' ');
 
-const element = (
-    <div title='Outer div'>
+
+class HelloWorld extends React.Component {
+  render() {
+    const continents = ['Africa', 'America', 'Asia', 'Australia', 'Europe'];
+    const helloContinents = Array.from(continents, c=> `Hello ${c}!`);
+    const message = helloContinents.join(' ');
+
+    return (
+      <div title='Outer div'>
         <h1 className='greeting'>{message}</h1>
-    </div>
-);				// JSX
+      </div>
+    )
+  }
+}
 
+// Instantiate an instance of HelloWorld, i.e. a React element
+const element = <HelloWorld />;
+
+// Still use ReactDOM.render to transform Virtual DOM to real DOM
 ReactDOM.render(element, document.getElementById('contents'));
